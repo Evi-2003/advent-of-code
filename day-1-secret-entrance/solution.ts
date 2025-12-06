4395f423f445c8989def973be3fa8aaa68e04890
Wammes <mail@eviwammes.nl>
2025-12-06 20:02:59 +0100

    day 1 - secret entrance

---
+++ day-1-secret-entrance/solution.ts
@@ -0,0 +1,31 @@
+import rotations from "./rotations.ts";
+
+const minRotation = 0;
+const maxRotation = 100; // maxRotation is 100 because we count 0-99
+
+let currentRotation = 50;
+let password = 0;
+
+rotations.forEach((rotation) => {
+  const amount = parseInt(rotation.substring(1));
+
+  if (rotation[0] === "L") {
+    currentRotation = currentRotation - amount;
+  }
+
+  while (currentRotation < 0) {
+    currentRotation = maxRotation - currentRotation * -1;
+  }
+
+  if (rotation[0] === "R") {
+    currentRotation = currentRotation + amount;
+  }
+
+  while (currentRotation > maxRotation - 1) {
+    currentRotation = minRotation + (currentRotation - maxRotation);
+  }
+
+  if (currentRotation === 0) {
+    password++;
+  }
+});
